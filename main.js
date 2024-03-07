0d0adbb356278707f726feadbd59fa9ea6c2d6cb
Samih Wadi <samihamaar@gmail.com>
2024-03-07 02:12:32 -0500

    Add script for navbar

---
+++ main.js
@@ -0,0 +1,11 @@
+var lastScrollTop = 0;
+var navbar = document.querySelector('.main-nav');
+window.addEventListener('scroll', function() {
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-80px";
+    } else {
+        navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+});
