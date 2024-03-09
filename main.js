74bb2e3d3e5ca6aec70f8362a7b7f5e58fd13d36
Samih Wadi <samihamaar@gmail.com>
2024-03-09 16:21:53 -0500

    Modify navbar on scroll

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
 var lastScrollTop = 0;
 var navbar = document.querySelector('.main-nav');
+const sidebar = document.querySelector('.sidebar');
+const topbar = document.querySelector('.topbar');
+const hamburger = document.querySelector('.menu-btn');
+const emailBtn = document.getElementById('email-btn');
+const timelines = document.querySelectorAll('.timeline')
+const timelinesContainer = document.querySelectorAll('.timeline-container');
 window.addEventListener('scroll', function() {
     var scrollTop = window.scrollY || document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
         navbar.style.top = "-80px";
     } else {
         navbar.style.top = "0";
+        navbar.style.background = "var(--color-bg)";
+    }
+    if (sidebar.style.display == 'flex') {
+        navbar.style.background = "transparent";
+    } else {
+        navbar.style.background = "var(--color-bg)";
     }
     lastScrollTop = scrollTop;
 });
 
-const sidebar = document.querySelector('.sidebar');
-const emailBtn = document.getElementById('email-btn');
 function showSidebar(){
+    navbar.style.background = "transparent";
     sidebar.style.display = 'flex';
     emailBtn.style.display = 'none';
+    hamburger.style.display = 'none';
+    logo.style.display = 'none';
+    topbar.style.flexBasis = '50%';
 }
 
 function hideSidebar(){
+    navbar.style.background = "var(--color-bg)";
     sidebar.style.display = 'none'; 
     emailBtn.style.display = 'flex';
+    hamburger.style.display = 'flex';
+    logo.style.display = 'inline';
+    topbar.style.flexBasis = '100%';
 }
-const timelines = document.querySelectorAll('.timeline')
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if(entry.isIntersecting) {
@@ -32,5 +50,4 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
-const timelinesContainer = document.querySelectorAll('.timeline-container');
 timelinesContainer.forEach(el => observer.observe(el));
